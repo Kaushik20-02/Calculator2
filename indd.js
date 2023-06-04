e7e6034797b244bbd246144e312670e1df52ce51
Kaushik20-02 <111055319+Kaushik20-02@users.noreply.github.com>
2023-06-04 07:45:05 +0530

    Add files via upload

---
+++ indd.js
@@ -0,0 +1,18 @@
+function dis(val)
+{
+    document.getElementById("answer").value+=val
+}
+
+//function that evaluates the digit and return result
+function solve()
+{
+    let x = document.getElementById("answer").value
+    let y = eval(x)
+    document.getElementById("answer").value = y
+}
+
+//function that clear the display
+function clr()
+{
+    document.getElementById("answer").value = ""
+}
